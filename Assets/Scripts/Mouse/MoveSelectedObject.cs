ec34c3016c79afa64dccec999b16c366c0952197
Anton Pashkevich <natoy7000@gmail.com>
2022-07-11 09:43:49 +0200

    Updated Move

--- Assets/Scripts/Mouse/MoveSelectedObject.cs
+++ Assets/Scripts/Mouse/MoveSelectedObject.cs
@@ -1,47 +1,7 @@
-using System;
 using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
-public enum EaseMove
-{
-    Unset,
-    Linear,
-    InSine,
-    OutSine,
-    InOutSine,
-    InQuad,
-    OutQuad,
-    InOutQuad,
-    InCubic,
-    OutCubic,
-    InOutCubic,
-    InQuart,
-    OutQuart,
-    InOutQuart,
-    InQuint,
-    OutQuint,
-    InOutQuint,
-    InExpo,
-    OutExpo,
-    InOutExpo,
-    InCirc,
-    OutCirc,
-    InOutCirc,
-    InElastic,
-    OutElastic,
-    InOutElastic,
-    InBack,
-    OutBack,
-    InOutBack,
-    InBounce,
-    OutBounce,
-    InOutBounce,
-    Flash,
-    InFlash,
-    OutFlash,
-    InOutFlash,
-}
 public class MoveSelectedObject : MonoBehaviour
 {
     [SerializeField] private float _speedMove = 5f;
@@ -49,7 +9,7 @@ public class MoveSelectedObject : MonoBehaviour
     [SerializeField] private float _minSpeedDown = 0.1f;
     [SerializeField] private float _distanceTouch = 5f;
     [SerializeField] private ManagerUIPanel _managerUIPanel;
-    [SerializeField] private EaseMove _ease = EaseMove.InSine;
+    [SerializeField] private Ease _ease = Ease.InSine;
     private Camera _camera;
     private SelectObject _currentSelectObject;
 
@@ -74,120 +34,7 @@ public class MoveSelectedObject : MonoBehaviour
     public void StartCoroutineMove(Vector3 position, bool isPlatform = false)
     {
         var objectTransform = _currentSelectObject.gameObject.transform;
-
-        switch (_ease)
-        {
-            case EaseMove.InSine:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InSine);
-                break;
-            case EaseMove.OutSine:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutSine);
-                break;
-            case EaseMove.InOutSine:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutSine);
-                break;
-            case EaseMove.InQuad:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InQuad);
-                break;
-            case EaseMove.Flash:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.Flash);
-                break;
-            case EaseMove.InFlash:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InFlash);
-                break;
-            case EaseMove.Unset:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.Unset);
-                break;
-            case EaseMove.Linear:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.Linear);
-                break;
-            case EaseMove.OutQuad:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutQuad);
-                break;
-            case EaseMove.InOutQuad:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutQuad);
-                break;
-            case EaseMove.InCubic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InCubic);
-                break;
-            case EaseMove.OutCubic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutCubic);
-                break;
-            case EaseMove.InOutCubic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutCubic);
-                break;
-            case EaseMove.InQuart:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InQuart);
-                break;
-            case EaseMove.OutQuart:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutQuart);
-                break;
-            case EaseMove.InOutQuart:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutQuart);
-                break;
-            case EaseMove.InQuint:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InQuint);
-                break;
-            case EaseMove.OutQuint:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutQuint);
-                break;
-            case EaseMove.InOutQuint:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutQuint);
-                break;
-            case EaseMove.InExpo:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InExpo);
-                break;
-            case EaseMove.OutExpo:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutExpo);
-                break;
-            case EaseMove.InOutExpo:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutExpo);
-                break;
-            case EaseMove.InCirc:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InCirc);
-                break;
-            case EaseMove.OutCirc:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutCirc);
-                break;
-            case EaseMove.InOutCirc:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutCirc);
-                break;
-            case EaseMove.InElastic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InElastic);
-                break;
-            case EaseMove.OutElastic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutElastic);
-                break;
-            case EaseMove.InOutElastic:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutElastic);
-                break;
-            case EaseMove.InBack:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InBack);
-                break;
-            case EaseMove.OutBack:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutBack);
-                break;
-            case EaseMove.InOutBack:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutBack);
-                break;
-            case EaseMove.InBounce:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InBounce);
-                break;
-            case EaseMove.OutBounce:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutBounce);
-                break;
-            case EaseMove.InOutBounce:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutBounce);
-                break;
-            case EaseMove.OutFlash:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.OutFlash);
-                break;
-            case EaseMove.InOutFlash:
-                objectTransform.DOMove(position, _speedDown).SetEase(Ease.InOutFlash);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+        objectTransform.DOMove(position, _speedDown).SetEase(_ease);
 
         //StopAllCoroutines();
         //     StartCoroutine(MoveDown(position, isPlatform));
@@ -212,8 +59,7 @@ public class MoveSelectedObject : MonoBehaviour
         _managerUIPanel.OpenPanelSummary(_currentSelectObject.Answer);
     }
     
-    
-
+    //Don't used 11.07
     private IEnumerator MoveDown(Vector3 position, bool isPlatform)
     {
         var objectTransform = _currentSelectObject.gameObject.transform;
