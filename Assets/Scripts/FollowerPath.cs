3937f59854cde31fb92be4c0b74d6d0791551833
Anton Pashkevich <natoy7000@gmail.com>
2022-07-12 17:55:39 +0200

    The movement of vans is ready

--- Assets/Scripts/FollowerPath.cs
+++ Assets/Scripts/FollowerPath.cs
@@ -5,18 +5,49 @@ public class FollowerPath : MonoBehaviour
 {
     [SerializeField] private PathCreator _pathCreator;
     [SerializeField] private float _speed = 5f;
-    
+    [SerializeField] private FollowerVan[] _followerVans;
+    [SerializeField] private float _speedVan = 5f;
+    [SerializeField] private Vector3 _offsetIocomotive = Vector3.zero; 
     private float _distanceTravelled;
+    private bool _isEndPath;
 
     private void Update()
     {
-        MoveToPath();
+        if (!_isEndPath)
+            MoveVans();
     }
 
-    private void MoveToPath()
+    private void MoveVans()
+    {
+        var previousPosition = transform.position;
+        var previousRotation = transform.rotation;
+        previousPosition += _offsetIocomotive; 
+        
+        foreach (var van in _followerVans)
+        {
+            var vanTransform = van.transform;
+            var tempRotation = vanTransform.rotation;
+            var tempPosition = vanTransform.position;
+            van.transform.rotation =
+                Quaternion.Lerp(vanTransform.rotation, previousRotation, _speedVan * Time.deltaTime);
+            van.transform.position = Vector3.Lerp(vanTransform.position, previousPosition, _speedVan * Time.deltaTime);
+
+            previousPosition = tempPosition;
+            previousRotation = tempRotation;
+        }
+
+        MoveHeadPath();
+    }
+
+    private void MoveHeadPath()
     {
         _distanceTravelled += _speed * Time.deltaTime;
-        transform.position = _pathCreator.path.GetPointAtDistance(_distanceTravelled);
-        transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+        var nextPosition = _pathCreator.path.GetPointAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+        
+        if (transform.position == nextPosition)
+            _isEndPath = true;
+        
+        transform.position = nextPosition;
+        transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
     }
 }
--- Assets/Scripts/FollowerVan.cs
+++ Assets/Scripts/FollowerVan.cs
@@ -1,14 +1,32 @@
+using System;
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class FollowerVan : MonoBehaviour
 {
-    [SerializeField] private Transform _transformFollower;
+ /*   [SerializeField] private Transform _transformFollower;
     [SerializeField] private float _speedRotation = 5f;
-    [SerializeField] private float _speedMove = 5f; 
+    [SerializeField] private float _speedMove = 5f;
+    [SerializeField] private float _distanceFollower = 1f;
+
+    private float _sphereRadius = 0.1f;
+
+    private void Start()
+    {
+        StartCoroutine(StartPostion()); 
+    }
 
     private void Update()
     {
-        MoveVan();
+        if (Vector3.Distance(_transformFollower.position, transform.position) > _distanceFollower)
+            MoveVan();
+    }
+
+    private IEnumerator StartPostion()
+    {
+        yield return new WaitForEndOfFrame();
+        transform.position = _transformFollower.position;
     }
 
     private void MoveVan()
@@ -18,4 +36,12 @@ public class FollowerVan : MonoBehaviour
             _speedRotation * Time.deltaTime);
         transform.Translate(Vector3.forward * _speedMove * Time.deltaTime);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(_transformFollower.position, _sphereRadius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, _sphereRadius);
+    }*/
 }
