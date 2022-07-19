c8ba56880b7dedbe8fba8b0be27a84afe9020e81
Anton Pashkevich <natoy7000@gmail.com>
2022-07-19 17:43:05 +0200

    Created new mechanism movement the train

---
+++ Assets/Scripts/uToys2/MoveTrain.cs
@@ -0,0 +1,84 @@
+using PathCreation;
+using UnityEngine;
+
+public class MoveTrain : MonoBehaviour
+{
+    [SerializeField] private PathCreator[] _pathCreator;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Wagon[] _wagons;
+
+    private float _distanceTravelled;
+    private bool _isEndPath;
+    private int _countPath;
+
+    private void Start()
+    {
+        StartPositionWagons();
+    }
+
+    private void Update()
+    {
+        if (!_isEndPath)
+            MoveWagonsAndHead();
+    }
+
+    private void MoveWagonsAndHead()
+    {
+        var previousPosition = transform.position;
+        var previousRotation = transform.rotation;
+
+        foreach (var wagon in _wagons)
+        {
+            var vanTransform = wagon.transform;
+            var tempRotation = vanTransform.rotation;
+            var tempPosition = vanTransform.position;
+            wagon.transform.rotation =
+                Quaternion.Lerp(vanTransform.rotation, previousRotation, _speed  * Time.deltaTime);
+            wagon.transform.position = Vector3.Lerp(vanTransform.position, previousPosition, _speed * Time.deltaTime);
+
+            previousPosition = tempPosition;
+            previousRotation = tempRotation;
+        }
+
+        MoveHeadPath();
+    }
+
+    private void MoveHeadPath()
+    {
+        _distanceTravelled += _speed * Time.deltaTime;
+        var nextPosition = _pathCreator[_countPath].path
+            .GetPointAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+
+        if (transform.position == nextPosition)
+            _isEndPath = true;
+
+
+        transform.position = nextPosition;
+        transform.rotation = _pathCreator[_countPath].path
+            .GetRotationAtDistance(_distanceTravelled, EndOfPathInstruction.Stop);
+    }
+
+    private void StartPositionWagons()
+    {
+        var position = _pathCreator[_countPath].path.GetPointAtDistance(0);
+        var rotation = _pathCreator[_countPath].path.GetRotationAtDistance(0);
+        var backOffset = Vector3.back;
+        foreach (var wagon in _wagons)
+        {
+            var transformWagon = wagon.transform;
+            transformWagon.position = position;
+            transformWagon.rotation = rotation;
+            transformWagon.Translate(backOffset);
+            backOffset.z -= 1;
+        }
+    }
+
+    [ContextMenu("NextPath")]
+    private void NextPath()
+    {
+        _countPath++;
+        if (_pathCreator.Length <= _countPath) return;
+        _distanceTravelled = 0f;
+        _isEndPath = false;
+    }
+}
