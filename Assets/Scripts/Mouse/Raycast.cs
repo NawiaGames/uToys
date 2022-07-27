ac1a314a2bbe213c782809d19462bd22c39f208c
Anton Pashkevich <natoy7000@gmail.com>
2022-07-27 16:03:05 +0200

    Updated (Scene, Base Level) Fixed Bugs

--- Assets/Scripts/Mouse/Raycast.cs
+++ Assets/Scripts/Mouse/Raycast.cs
@@ -25,10 +25,8 @@ public class Raycast
 
     public Platform RaycastPlatform()
     {
-        var layerMask = 1 << 2 | 1 << 9;
-        layerMask = ~layerMask;
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out var raycastHitInfo, Mathf.Infinity, layerMask );
+        Physics.Raycast(ray, out var raycastHitInfo, Mathf.Infinity, LayerMask.GetMask("SetSelectObject") );
         Debug.DrawRay(ray.origin, ray.direction * 1000f);
         if (raycastHitInfo.collider == null) return null;
 
@@ -40,7 +38,7 @@ public class Raycast
     public Vector3 GetInputPlanePosition()
     {
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out var raycastHitInfo, Mathf.Infinity, LayerMask.GetMask("InputPlane"));
+        Physics.Raycast(ray, out var raycastHitInfo, Mathf.Infinity,_layerMaskInputPlane);
 
         if (raycastHitInfo.collider == null) return Vector3.zero;
         if (raycastHitInfo.collider.GetComponent<InputPlane>() == null) return Vector3.zero;
